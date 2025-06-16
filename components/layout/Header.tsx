72cbce7c1ecc43e291bfb1cfbc39656e4c686a5d
NielsEngelhard <engelhardniels@gmail.com>
2025-06-16 15:37:51 +0200

    WIP

---
+++ components/layout/Header.tsx
@@ -0,0 +1,21 @@
+import { APP_NAME } from "@/lib/global-constants";
+
+export default function Header() {
+    return (
+        <header className="h-[65px] bg-white/95 backdrop-blur-sm border-b border-gray-200 sticky top-0 z-50 flex justify-center w-full">
+            <div className="max-w-7xl flex justify-between w-full items-center">
+                {/* Left */}
+                <div className="text-2xl font-bold bg-gradient-to-r from-primary to-secondary bg-clip-text text-transparent">
+                    <a href="/">
+                        {APP_NAME}
+                    </a>
+                </div>
+
+                {/* Right */}
+                <div>
+
+                </div>
+            </div>
+        </header>
+    )
+}
