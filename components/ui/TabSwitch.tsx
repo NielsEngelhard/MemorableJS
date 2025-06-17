67cc8a0cd7d9c73d5f8063d67597bd32a2d044b0
NielsEngelhard <engelhardniels@gmail.com>
2025-06-17 15:21:36 +0200

    create login form

--- components/ui/TabSwitch.tsx
+++ components/ui/TabSwitch.tsx
@@ -4,13 +4,21 @@ export type Tab = {
 }
 
 interface Props {
+    activeTabIndex: number;
     tabs: Tab[];
+    onTabChange: (index: number) => void;
 }
 
-export default function TabSwitch({ tabs }: Props) {
+export default function TabSwitch({ tabs, activeTabIndex, onTabChange }: Props) {
     return (
-        <div className="w-full bg-foreground-muted/5 rounded-sm">
-            lol
+        <div className="w-full bg-foreground-muted/5 rounded-sm flex flex-row">
+            {tabs.map((item, index) => (
+                <button
+                    onClick={() => onTabChange(index)}
+                    className={`flex-1 text-center m-1 p-1 cursor-pointer text-sm font-semibold ${index == activeTabIndex ? 'bg-background' : 'text-foreground-muted/75'}`}>
+                    {item.label}
+                </button>
+            ))}
         </div>
     )
 }
--- features/auth/components/AuthModal.tsx
+++ features/auth/components/AuthModal.tsx
@@ -6,6 +6,8 @@ import LoginForm from "./LoginForm";
 import TabSwitch, { Tab } from "@/components/ui/TabSwitch";
 import FadedText from "@/components/ui/text/FadedText";
 import { APP_NAME } from "@/lib/global-constants";
+import { useState } from "react";
+import SignUpForm from "./SignUpForm";
 
 const tabs: Tab[] = [
     {
@@ -20,20 +22,24 @@ const tabs: Tab[] = [
 
 export default function AuthModal() {
     const { showAuthModal, toggleShowAuthModal } = useAuth();
+    const [activeTabIndex, setActiveTabIndex] = useState(0);
 
     return (
         <Modal isVisible={showAuthModal} onClose={toggleShowAuthModal}>
-            
-            <div className="font-bold text-xl text-center">
-                <FadedText>Welcome to {APP_NAME}</FadedText>
-            </div>
+            <div className="flex flex-col gap-3">
+                <div className="font-bold text-xl text-center">
+                    <FadedText>Welcome to {APP_NAME}</FadedText>
+                </div>
 
-            <TabSwitch tabs={tabs}>
+                <TabSwitch tabs={tabs} activeTabIndex={activeTabIndex} onTabChange={(index) => setActiveTabIndex(index)} />
+                
+                {activeTabIndex == 0 &&
+                    <LoginForm></LoginForm>
+                }
 
-            </TabSwitch>
-            
-            <div>
-                <LoginForm></LoginForm>
+                {activeTabIndex == 1 &&
+                    <SignUpForm></SignUpForm>
+                }
             </div>
         </Modal>
     )
