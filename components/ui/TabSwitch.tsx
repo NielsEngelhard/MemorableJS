d781f05a46e1e9c90b061e78238b91acda76de09
NielsEngelhard <engelhardniels@gmail.com>
2025-06-17 16:15:33 +0200

    WIP

--- components/ui/TabSwitch.tsx
+++ components/ui/TabSwitch.tsx
@@ -14,6 +14,7 @@ export default function TabSwitch({ tabs, activeTabIndex, onTabChange }: Props) 
         <div className="w-full bg-foreground-muted/5 rounded-sm flex flex-row">
             {tabs.map((item, index) => (
                 <button
+                    key={index}
                     onClick={() => onTabChange(index)}
                     className={`flex-1 text-center m-1 p-1 cursor-pointer text-sm font-semibold ${index == activeTabIndex ? 'bg-background' : 'text-foreground-muted/75'}`}>
                     {item.label}
