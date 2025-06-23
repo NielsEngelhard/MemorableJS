c3a5043dd6cb248184e1dc5297d30fd0af955d1d
NielsEngelhard <engelhardniels@gmail.com>
2025-06-23 16:21:25 +0200

    rm migration

--- drizzle/migrations/0001_famous_zombie.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "letter_league_game" ALTER COLUMN "currentRound" SET DEFAULT 1;--> statement-breakpoint
-ALTER TABLE "letter_league_game" ADD COLUMN "wordLength" integer NOT NULL;--> statement-breakpoint
-ALTER TABLE "letter_league_game" ADD COLUMN "currentGuess" integer DEFAULT 1 NOT NULL;
