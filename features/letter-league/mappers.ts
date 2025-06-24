5dfc4ad9a69ee7f193f95c2b60571f65b48dee46
NielsEngelhard <engelhardniels@gmail.com>
2025-06-24 14:55:25 +0200

    working

--- features/letter-league/mappers.ts
+++ features/letter-league/mappers.ts
@@ -5,14 +5,14 @@ export default function MapLetterLeagueGameFromDb(game: DbLetterLeagueGame): Let
     return {
         id: game.id,
         currentRound: game.currentRound,
+        currentGuess: game.currentGuess,
         gameMode: game.gameMode,
         maxAttemptsPerRound: game.maxAttemptsPerRound,
         timePerTurn: game.timePerTurn,
         totalRounds: game.totalRounds,
         userHostId: game.userHostId,
         wordLength: game.words[0].word.length,
-        guesses: [],
+        rounds: game.rounds,
         createdAt: game.createdAt,
-        firstLetter: game.words.find(w => w.round == game.currentRound)?.word[0] ?? "?"
     }
 }
