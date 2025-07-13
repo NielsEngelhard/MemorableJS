61cc9655888d7e2a2fcd4f30f2192b852b707897
engelhardniels <engelhardniels@gmail.com>
2025-07-13 14:15:21 +0200

    add more score tests

--- tests/word/score-calculator/calculcate_totalScore.test.ts
+++ tests/word/score-calculator/calculcate_totalScore.test.ts
@@ -1,6 +1,6 @@
 import { LetterState } from "@/drizzle/schema/enum/letter-state";
 import { ScoreCalculator } from "@/features/score/score-calculator";
-import { CALCULATE_STREAK_POINTS, INSTANT_CORRECT_POINTS, INSTANT_GUESS_BONUS, POINTS_PER_STREAK_ITEM, SECOND_GUESS_BONUS, JUST_A_GUESS_BONUS } from "@/features/score/score-constants";
+import { CALCULATE_STREAK_POINTS, INSTANT_CORRECT_POINTS, INSTANT_GUESS_BONUS, POINTS_PER_STREAK_ITEM, SECOND_GUESS_BONUS, JUST_A_GUESS_BONUS, MISPLACED_POINTS, CORRECT_AFTER_MISPLACED_POINTS } from "@/features/score/score-constants";
 import { ValidatedLetter } from "@/features/word/word-models";
 
 describe("calculate total score", () => {
@@ -27,5 +27,53 @@ describe("calculate total score", () => {
         expect(score.totalScore).toEqual(expectedScore);
     });
 
-  
+    it("should have correct total score scenario 'JUST A NORMAL GUESS WITH SOME CORRECT SOME MISPLACED AND SOME WRONG'", () => {
+        const newCorrectLetters: ValidatedLetter[] = [
+            { letter: "B", position: 2, state: LetterState.Correct },
+            { letter: "C", position: 5, state: LetterState.Misplaced },
+            { letter: "D", position: 4, state: LetterState.Misplaced },
+            { letter: "Q", position: 7, state: LetterState.Wrong },
+        ];
+
+        const expectedScore = 
+            1 * INSTANT_CORRECT_POINTS // CORRECT LETTERSTATE
+            + 2 * MISPLACED_POINTS;    // MISPLACED LETTERSTATE
+
+        const score = ScoreCalculator.calculate({
+            currentGuessIndex: 2,
+            wordGuessed: false,
+            newLetters: newCorrectLetters,
+            previouslyGuessedLetters: [{ letter: "A", position: 1, state: LetterState.Correct }],
+        });
+
+        expect(score.totalScore).toEqual(expectedScore);
+    });    
+
+      it("should have correct total score scenario 'JUST A NORMAL GUESS WITH SOME CORRECT SOME MISPLACED AND SOME WRONG AND A STREAK OF 4 AND ONE CORRECT AFTER MISPLACED'", () => {
+        const newCorrectLetters: ValidatedLetter[] = [
+            { letter: "B", position: 1, state: LetterState.Correct },
+            { letter: "Z", position: 12, state: LetterState.Misplaced },
+            { letter: "C", position: 3, state: LetterState.Correct },
+            { letter: "Z", position: 9, state: LetterState.Misplaced },
+            { letter: "J", position: 55, state: LetterState.Wrong },
+            { letter: "O", position: 9, state: LetterState.Wrong },
+            { letter: "D", position: 2, state: LetterState.Correct },
+            { letter: "Q", position: 4, state: LetterState.Correct },
+        ];
+
+        const expectedScore = 
+            2 * INSTANT_CORRECT_POINTS           // INSTANT CORRECT LETTERSTATE
+            + 2 * MISPLACED_POINTS               // MISPLACED LETTERSTATE
+            + 2 * CORRECT_AFTER_MISPLACED_POINTS // CORRECT AFTER MISPLACED LETTERSTATE
+            + CALCULATE_STREAK_POINTS(4);        // STREAK BONUS
+
+        const score = ScoreCalculator.calculate({
+            currentGuessIndex: 2,
+            wordGuessed: false,
+            newLetters: newCorrectLetters,
+            previouslyGuessedLetters: [{ letter: "B", state: LetterState.Misplaced }, { letter: "Q", state: LetterState.Misplaced }],
+        });
+
+        expect(score.totalScore).toEqual(expectedScore);
+    }); 
 });
